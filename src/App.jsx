68343615c5c196b3a9dce14e4dc8293861e72cf8
Avijeet pandey <avijeetpandey25@gmail.com>
2023-12-09 15:02:25 +0530

    Add: components and rediux toolkit

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,16 @@
 import "./App.css";
+import AddTodo from "./components/AddTodo";
+import Todos from "./components/Todos";
 
 function App() {
-  return <>React Application</>;
+  return (
+    <>
+      <h1>Learn about Redux toolkit</h1>
+
+      <AddTodo />
+      <Todos />
+    </>
+  );
 }
 
 export default App;
